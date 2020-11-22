9f73ae45976cace867893f97d03ccb464a6fd76c
wajibdiisi <wajibdiisi@gmail.com>
2020-11-22 16:00:11 +0700

    Update ProfileManager.php

--- app/Models/ProfileManager.php
+++ app/Models/ProfileManager.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
-class profileManager extends Eloquent
+class ProfileManager extends Eloquent
 {
     protected $connection = 'mongodb';
     protected $collection = 'profileManager';
